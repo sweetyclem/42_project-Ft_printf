55ed4163e78ff215c6c10e6af6fe03411ead10ab
Clementine PIRLOT <cpirlot@e1r11p12.42.fr>
2018-01-08 15:49:32 +0100

    get pound flag

--- get_flags.c
+++ get_flags.c
@@ -6,7 +6,7 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 15:31:23 by cpirlot           #+#    #+#             */
-/*   Updated: 2018/01/08 15:43:19 by cpirlot          ###   ########.fr       */
+/*   Updated: 2018/01/08 15:46:39 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,20 @@ int	get_minus_flag(char *str)
 	while (str[i] != '\0')
 	{
 		if (str[i] == '-')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	get_pound_flag(char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '#')
 			return (1);
 		i++;
 	}
