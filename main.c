defefcc8c6c11eeec76232e17668e738181fd73d
Clementine PIRLOT <cpirlot@e1r11p12.42.fr>
2018-01-10 07:54:12 +0100

    fix norm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/09 09:11:23 by cpirlot           #+#    #+#             */
-/*   Updated: 2018/01/09 17:13:53 by cpirlot          ###   ########.fr       */
+/*   Updated: 2018/01/10 07:50:44 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(void)
 	ft_putendl("mienne : ");
 	ft_putnbrendl(ft_printf("{% S}\n", L"(null)"));
 	ft_putendl("vraie printf : ");
-	ft_putnbrendl(printf("{% S", L"(null)"));
+	ft_putnbrendl(printf("{% S\n", L"(null)"));
 
 	ft_putendl("\n\nprintf(\"%5%\"))");
 	ft_putendl("mienne : ");
@@ -33,22 +33,4 @@ int	main(void)
 	ft_putnbrendl(ft_printf("%05c\n", 42));
 	ft_putendl("vraie printf : ");
 	ft_putnbrendl(printf("%05c\n", 42));
-
-	ft_putendl("\n\nprintf(\"%u\", UINT_MAX)");
-	ft_putendl("mienne : ");
-	ft_putnbrendl(ft_printf("%u\n", UINT_MAX));
-	ft_putendl("vraie printf : ");
-	ft_putnbrendl(printf("%u\n", UINT_MAX));
-
-	ft_putendl("\n\nprintf(\"%U\", ULONG_MAX / 2)");
-	ft_putendl("mienne : ");
-	ft_putnbrendl(ft_printf("%U\n", ULONG_MAX / 2));
-	ft_putendl("vraie printf : ");
-	ft_putnbrendl(printf("%U\n", ULONG_MAX / 2));
-
-	ft_putendl("\n\nprintf(\"%U\", ULONG_MAX))");
-	ft_putendl("mienne : ");
-	ft_putnbrendl(ft_printf("%U\n", ULONG_MAX));
-	ft_putendl("vraie printf : ");
-	ft_putnbrendl(printf("%U\n", ULONG_MAX));
 }
