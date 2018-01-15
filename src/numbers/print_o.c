10fb92aa2a8967fbbe6ee462214318b8bafabbc3
Clementine PIRLOT <cpirlot@e1r11p12.42.fr>
2018-01-15 16:18:08 +0100

    fix precision error for unsigned o and O

--- src/numbers/print_o.c
+++ src/numbers/print_o.c
@@ -6,7 +6,7 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/10 09:22:28 by cpirlot           #+#    #+#             */
-/*   Updated: 2018/01/15 16:01:57 by cpirlot          ###   ########.fr       */
+/*   Updated: 2018/01/15 16:15:59 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,7 @@ int	print_o_longlong(int char_written, t_format format, long long nb)
 		char_written += nb_len;
 	char_written += print_pound(format, nb);
 	if (format.precision > 0)
-		prec = format.precision -
-		(nb < 0 ? nb_len - 1 : nb_len);
+		prec = format.precision - (nb < 0 ? nb_len - 1 : nb_len);
 	char_written += print_width(format, char_written + (prec > 0 ? prec : 0));
 	char_written += print_zero_padding(format, char_written);
 	char_written += print_precision(format, nb, nb_len);
@@ -57,12 +56,19 @@ int	print_o_longlong(int char_written, t_format format, long long nb)
 
 int	print_u_o_longlong(int char_written, t_format format, unsigned long long nb)
 {
+	int	prec;
+	int	nb_len;
+
+	prec = 0;
+	nb_len = ft_u_len_base(nb, 8);
 	if (!(format.precision == -1 && nb == 0))
-		char_written += ft_u_len_base(nb, 8);
+		char_written += nb_len;
 	char_written += print_pound(format, nb);
-	char_written += print_width(format, char_written);
+	if (format.precision > 0)
+		prec = format.precision - nb_len;
+	char_written += print_width(format, char_written + (prec > 0 ? prec : 0));
 	char_written += print_zero_padding(format, char_written);
-	char_written += print_precision(format, nb, ft_len_base(nb, 8));
+	char_written += print_precision(format, nb, nb_len);
 	if (!(format.precision == -1 && nb == 0))
 		ft_put_u_longlong_base(nb, 8);
 	char_written += print_width_minus(format, char_written);
