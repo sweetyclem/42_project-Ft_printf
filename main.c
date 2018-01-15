a70047cc54a2faaab066c2e995199811eb8f6220
Clementine PIRLOT <cpirlot@e1r11p12.42.fr>
2018-01-15 10:15:05 +0100

    handle 0 padding with negative numbers

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/09 09:11:23 by cpirlot           #+#    #+#             */
-/*   Updated: 2018/01/15 09:05:07 by cpirlot          ###   ########.fr       */
+/*   Updated: 2018/01/15 10:14:38 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 
 int	main(void)
 {
-	ft_putendl("printf(\"%zd\", LLONG_MAX))");
+	ft_putendl("printf(\"{%o}\", -42))");
 	ft_putendl("mienne : ");
-	ft_putnbrendl(ft_printf("%zd\n", LLONG_MAX));
+	ft_putnbrendl(ft_printf("{%o}\n", -42));
 	ft_putendl("vraie printf : ");
-	ft_putnbrendl(printf("%zd\n", LLONG_MAX));
-	ft_putendl("\n\nprintf(\"%zu, %zu\", 0, ULLONG_MAX)");
+	ft_putnbrendl(printf("{%o}\n", -42));
+	ft_putendl("\n\nprintf(\"%lo\", (long)INT_MIN - 1)");
 	ft_putendl("mienne : ");
-	ft_putnbrendl(ft_printf("%zu, %zu\n", 0, ULLONG_MAX));
+	ft_putnbrendl(ft_printf("%lo\n", (long)INT_MIN - 1));
 	ft_putendl("vraie printf : ");
-	ft_putnbrendl(printf("%zu, %zu\n", 0, ULLONG_MAX));
+	ft_putnbrendl(printf("%lo\n", (long)INT_MIN - 1));
 }
--- src/numbers/print_di.c
+++ src/numbers/print_di.c
@@ -6,20 +6,37 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/10 09:16:39 by cpirlot           #+#    #+#             */
-/*   Updated: 2018/01/15 08:31:46 by cpirlot          ###   ########.fr       */
+/*   Updated: 2018/01/15 10:12:15 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../printf.h"
 
-int	print_d_longlong(int char_written, t_format format, long long nb)
+static void	ft_put_longlong_d(long long nbr)
+{
+	unsigned long long nb;
+
+	if (nbr < 0)
+	{
+		if (nbr != LONG_LONG_MIN)
+			nbr *= -1;
+	}
+	nb = nbr;
+	if (nb >= (unsigned long long)10)
+		ft_put_longlong_d(nb / 10);
+	ft_putchar(nb % 10 + '0');
+}
+
+int			print_d_longlong(int char_written, t_format format, long long nb)
 {
 	char_written += print_plus(format, nb);
 	char_written += print_space(format, nb);
 	char_written += ft_longlong_len(nb);
 	char_written += print_width(format, char_written);
+	if (nb < 0)
+		ft_putchar('-');
 	char_written += print_zero_padding(format, char_written);
-	ft_put_longlong_base(nb, 10);
+	ft_put_longlong_d(nb);
 	char_written += print_width_minus(format, char_written);
 	return (char_written);
 }
