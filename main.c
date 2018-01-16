5e6bc496149a5a3e934a1efdebddfe5a4d535bf6
Clementine PIRLOT <cpirlot@e1r12p14.42.fr>
2018-01-16 16:48:00 +0100

    update main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/09 09:11:23 by cpirlot           #+#    #+#             */
-/*   Updated: 2018/01/16 16:34:58 by cpirlot          ###   ########.fr       */
+/*   Updated: 2018/01/16 16:43:37 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 
 int	main(void)
 {
-	ft_putendl("printf(\"{%4.s}\", 42)");
+	ft_putendl("printf(\"{%05.c}\", 0)");
 	ft_putendl("mienne : ");
-	ft_putnbrendl(ft_printf("{%4.s}\n", "42"));
+	ft_putnbrendl(ft_printf("{%05.c\n}", 0));
 	ft_putendl("vraie printf : ");
-	ft_putnbrendl(printf("{%4.s}\n", "42"));
+	ft_putnbrendl(printf("{%05.c\n}", 0));
 }
