fb6662d68f3c4288b231cb857b63f4b0ae4cc0e7
Clementine PIRLOT <cpirlot@e1r12p14.42.fr>
2018-01-17 18:21:40 +0100

    update main with last error

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/09 09:11:23 by cpirlot           #+#    #+#             */
-/*   Updated: 2018/01/17 17:14:11 by cpirlot          ###   ########.fr       */
+/*   Updated: 2018/01/17 18:12:31 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,14 @@
 int	main(void)
 {
 	ft_putendl("---UNDEFINED BEHAVIOR---");
-	ft_putendl("printf(\"%###-#0000 33...12..#0+0d\", 256)");
+	ft_putendl("printf(\"{%###-#0000 33...12..#0+0d}\", 256)");
 	ft_putendl("mienne : ");
-	ft_putnbrendl(ft_printf("%###-#0000 33...12..#0+0d\n", 256));
+	ft_putnbrendl(ft_printf("{%###-#0000 33...12..#0+0d}\n", 256));
 	ft_putendl("vraie printf : ");
-	ft_putnbrendl(printf("%###-#0000 33...12..#0+0d\n", 256));
+	ft_putnbrendl(printf("{%###-#0000 33...12..#0+0d}\n", 256));
+	ft_putendl("printf(\"{%.12.#0d}\", 256)");
+	ft_putendl("mienne : ");
+	ft_putnbrendl(ft_printf("{%.12.#0d}\n", 256));
+	ft_putendl("vraie printf : ");
+	ft_putnbrendl(printf("{%.12.#0d}\n", 256));
 }
